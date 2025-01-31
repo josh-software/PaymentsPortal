f5d0a1fb6a2d50cdf542e8654228dca7ba1fdd7e
Joshua Skidmore <81509399+josh-software@users.noreply.github.com>
2025-01-31 17:45:08 +0000

    Added SetAccountFreeze

--- PaymentsPortal.Server/Controllers/AccountsController.cs
+++ PaymentsPortal.Server/Controllers/AccountsController.cs
@@ -17,17 +17,21 @@ namespace PaymentsPortal.Server.Controllers
 
         private readonly IRequestClient<CreateAccountRequest> _createAccountClient;
 
+        private readonly IRequestClient<SetAccountFreezeRequest> _setAccountFreezeClient;
+
         private readonly ILogger<AccountsController> _logger;
 
         public AccountsController(
             IRequestClient<GetAccountsRequest> getAccountsClient,
             IRequestClient<GetAccountByIdRequest> getAccountByIdClient,
             IRequestClient<CreateAccountRequest> createAccountClient,
+            IRequestClient<SetAccountFreezeRequest> setAccountFreezeClient,
             ILogger<AccountsController> logger)
         {
             _getAccountsClient = getAccountsClient;
             _getAccountByIdClient = getAccountByIdClient;
             _createAccountClient = createAccountClient;
+            _setAccountFreezeClient = setAccountFreezeClient;
             _logger = logger;
         }
 
@@ -73,6 +77,23 @@ namespace PaymentsPortal.Server.Controllers
                 HttpStatusCode.Created => CreatedAtAction(nameof(GetById), new { id = response.Message.Account.Id }, response.Message.Account),
                 HttpStatusCode.Conflict => Conflict(response.Message.Message),
                 HttpStatusCode.BadRequest => BadRequest(response.Message.Message),
+                _ => StatusCode((int)response.Message.StatusCode, response.Message.Message)
+            };
+        }
+
+        [HttpPut("{id}/freeze", Name = "SetAccountFreeze")]
+        public async Task<IActionResult> Freeze(Guid id, [FromBody] bool isFrozen)
+        {
+            var response = await _setAccountFreezeClient
+                .GetResponse<SetAccountFreezeResponse>(new SetAccountFreezeRequest
+                {
+                    Id = id,
+                    IsFrozen = isFrozen
+                });
+            return response.Message.StatusCode switch
+            {
+                HttpStatusCode.OK => Ok(response.Message.Message),
+                HttpStatusCode.NotFound => NotFound(response.Message.Message),
                 _ => StatusCode((int)response.Message.StatusCode, response.Message.Message)
             };
         }
---
+++ Services/PaymentsPortal.Services.Accounts/Consumers/SetAccountFreezeConsumer.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using PaymentsPortal.Common.Contracts;
+using PaymentsPortal.Services.Accounts.Data;
+
+namespace PaymentsPortal.Services.Accounts.Consumers
+{
+    public class SetAccountFreezeConsumer : IConsumer<SetAccountFreezeRequest>
+    {
+        private AccountsDbContext _dbContext;
+
+        public SetAccountFreezeConsumer(AccountsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Consume(ConsumeContext<SetAccountFreezeRequest> consumer)
+        {
+            var accountId = consumer.Message.Id;
+            var account = await _dbContext.Accounts
+                .FirstOrDefaultAsync(a => a.Id == accountId);
+
+            if (account == null)
+            {
+                await consumer.RespondAsync(
+                    new SetAccountFreezeResponse
+                    {
+                        StatusCode = HttpStatusCode.NotFound,
+                        Message = "Account not found"
+                    }
+                );
+                return;
+            }
+
+            if (account.IsFrozen == consumer.Message.IsFrozen)
+            {
+                await consumer.RespondAsync(
+                    new SetAccountFreezeResponse
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        Message = "Account freeze status not changed"
+                    }
+                );
+                return;
+            }
+
+            account.IsFrozen = consumer.Message.IsFrozen;
+            await _dbContext.SaveChangesAsync();
+            await consumer.RespondAsync(
+                new SetAccountFreezeResponse
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Message = "Account freeze status updated"
+                }
+            );
+        }
+    }
+}
